d78b2db1a79bf19d426a544be3bfe652873d21b6
qianlicaody <14061115@buaa.edu.cn>
2018-09-06 12:51:48 +0800

    upload .\eccv2018_download.py to get all ECCV 2018 papers

--- eccv2018_download.py
+++ eccv2018_download.py
@@ -5,10 +5,10 @@
  * @Date: 2018-09-06 10:24:03 
  * @Last Modified by:   shifaqiang 
  * @Last Modified time: 2018-09-06 10:24:03 
- * @Desc: a simple shell for downloading all ECCV 2018 papers.
-            This is referred from CVer(Wechat public account)
+ * @Desc: a simple shell for downloading all ECCV 2018 papers. This is referred from CVer(Wechat public account)
             
         There are all papers in Baidu Netdisk which are download by this shell :
+            href_link: https://pan.baidu.com/s/1b6XSZimpdqrzHOc891m48g password: cpuc
 '''
 
 from multiprocessing import Pool
@@ -29,7 +29,7 @@ def get_pdf(data):
         print("This file already exists, skip {}".format(name))
         return
     try:
-        print(prefix+href)
+        # print(prefix+href)
         content = requests.get(prefix+href).content()
         with open(directory+"{}.pdf".format(name), "wb") as f:
             f.write(content)
@@ -39,7 +39,7 @@ def get_pdf(data):
         # print(traceback.format_exc())
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     if not os.path.exists(directory):
         os.mkdir(directory)
 
@@ -53,8 +53,18 @@ if __name__=="__main__":
         if item.text.strip() == "pdf":
             href = item.get("href").strip()
             pdf_list.append(href)
-    assert len(pdf_list) == len(title_list), "numbers of title and pdf not equal"
-    print("Find {} papers".format(len(pdf_list)))
+    number = len(pdf_list)
+    assert number == len(title_list), "numbers of title and pdf not equal"
 
-    pool = Pool(10)
-    pool.map(get_pdf, zip(pdf_list, title_list))
+    print("Find {} papers".format(number))
+
+    # pool = Pool(10)
+    # pool.map(get_pdf, zip(pdf_list, title_list))
+    '''
+        If you have internet access without GFW(Great Fire Wall) or you have a VPN ladder,
+    you can uncomment pool function to accelerate your download process by using multi-process method.
+    '''
+    for i in range(number):
+        get_pdf((pdf_list[i], title_list[i]))
+
+    print("That's all!")
